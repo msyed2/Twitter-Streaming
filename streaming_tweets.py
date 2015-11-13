57c822ca24309eafe03b314f10b69597f471ba6f
munirasyed <roseblooms4ever@gmail.com>
2015-11-13 17:05:32 -0500

    streaming_tweets
    Run as:
    python streaming_tweets.py > tweets.txt

---
+++ streaming_tweets.py
@@ -0,0 +1,33 @@
+#Import the necessary methods from tweepy library
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+#Add the appropriate values to the following variables that contains the user credentials to access Twitter API 
+access_token = "Access token"
+access_token_secret = "Access token secret"
+consumer_key = "Consumer key"
+consumer_secret = "Consumer secret"
+
+
+#Prints tweets to stdout.
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print data
+        return True
+
+    def on_error(self, status):
+        print status
+
+
+if __name__ == '__main__':
+
+    #Twitter authentication and Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+
+    #Twitter steam will have keywords: 'elections', 'Trump', 'Carson'
+    stream.filter(track=['elections', 'Trump', 'Carson'])
